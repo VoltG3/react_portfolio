dea4d7a10a6d9c10f390f197f844011a19d104bd
VoltG3 <kristians.kodehode@gmail.com>
2022-08-29 14:36:30 +0200

    [ UPDATE ] - section info

--- src/sections/SectionInfo.js
+++ src/sections/SectionInfo.js
@@ -1,9 +1,32 @@
+import styled from "styled-components";
 import { StyledSectionInfoContainer } from './Sections.styles'
 
 export default function SectionInfo() {
     return (
         <StyledSectionInfoContainer>
-            <p>Info</p>
+            <div>
+                <p className="sectionInfo-textline-textcontent">
+                    Jeg har vært opptatt av IT siden middelalderen: MS-DOS / Win 3.1
+                </p>
+
+                <p className="sectionInfo-textline-textcontent">
+                    På tiden lærer jeg mye på egen hånd om JavaScript og Java. Jeg
+                    liker å samarbeide i gruppe. Fordypning inn i stadige utfordringer
+                    i problemløsninger.
+                </p>
+
+                <p className="sectionInfo-textline-textcontent">
+                    Mine hobbyer er historie før steinalderen, eksperimentell arkeologi
+                    og å spille munnharpa.
+                </p>
+
+                <p className="sectionInfo-textline-textcontent">
+                    Jeg trivers med å utforske det metanivået av programmer samt
+                    interaksjoner og sammenheger gjennom de. Har hat diverse basic
+                    kurs inni i BackEnd, CLI-Linux. Ser frem etter en miljø hvor
+                    jeg kan bidra og etterhvert vokse til FullStack.
+                </p>
+            </div>
         </StyledSectionInfoContainer>
     )
 }
--- src/sections/Sections.styles.js
+++ src/sections/Sections.styles.js
@@ -10,9 +10,16 @@ export const StyledSectionHeaderContainer = styled.div`
 `
 
 export const StyledSectionInfoContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
   width: 1440px;
   height: 500px;
   border: solid 1px black;
+  
+  div {
+    padding: 0 87px 0 87px;
+  }
 `
 
 export const StyledSectionDevContainer = styled.div`
