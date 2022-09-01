f38906dc987834fdb23f9eb57f299194c71e2268
VoltG3 <kristians.kodehode@gmail.com>
2022-09-01 19:11:58 +0200

    [ UPDATE ] - redirect to landing page

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import {BrowserRouter, Routes, Route, Navigate} from 'react-router-dom'
 import App from './App'
 import Default from './routes/Default'
 import InfoExtension from './routes/InfoExtension'
@@ -16,6 +16,7 @@ root.render(
                 <Route path="infoextension" element={<InfoExtension />} />
                 <Route path="devextension" element={<DevExtension />} />
                 <Route path="designextension" element={<DesignExtension />} />
+                <Route path="/" element={<Navigate replace to="default" />} />
             </Route>
         </Routes>
     </BrowserRouter>
